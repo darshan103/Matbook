c99b7df0d3181824c283e5e91f1a28a7abec880d
darshan103 <singhdarshan2166@gmail.com>
2025-11-28 09:37:54 +0530

    routed to employeeform

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
-import Employeeform from './components/EmployeeForm';
+import Employeeform from './components/Employeeform';
+
+function Home() {
+  const navigate = useNavigate();
+  function handleEmployeeForm() {
+    navigate('/api/form-schema');
+  }
+
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <button
+        onClick={handleEmployeeForm}
+        className="rounded-none border border-black m-2 p-2 hover:bg-violet-800 hover:text-white"
+      >
+        Onboard Employee
+      </button>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className='flex items-center justify-center'>
-        <button className="rounded-none border border-black m-2 p-2 hover:bg-violet-800 hover:text-white">Onboard Employee</button>
-      </div>
-
+    <>
       <Routes>
-        <Route path="" element={<Employeeform />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/api/form-schema" element={<Employeeform />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
